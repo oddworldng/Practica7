cb90c8c8abdff2b78d4b3428482a569a5f277b4f
oddworldng <el.lobo.4ever@gmail.com>
2012-11-01 20:32:04 +0000

    commit

--- lib/nr.rb
+++ lib/nr.rb
@@ -2,16 +2,56 @@
 
 class NumerosRacionales
 
-	def numerador
-		@numerador = 1
+	attr_accessor :numerador, :denominador
+
+	def initialize(x,y)
+		@numerador = x
+		@denominador = y
 	end
 
-	def denominador
-		@numerador = 2
+	def num
+		numerador
+	end
+
+	def denom
+		denominador
 	end
 
 	def numeroracional
-		return (@numerador/@denominador)
+		return numerador/denominador
+	end
+	
+	def mostrarab # Muestra la fracción en forma a/b
+		puts "#{numerador}/#{denominador}"
+	end
+
+	def mostrarfloat 
+		puts numeroracional
+	end
+
+	def comprobarfracciones
+		if ((3.5/2.5) == (3.5/2.5))
+			return "Son iguales"
+		else
+			return "Son distintas"
+		end
+	end
+
+	def vabsoluto
+		return numeroracional.abs
+	end
+	
+	def reciproco
+		return 1/numeroracional
+	end
+
+	def -(a,b)
+		return @denominador
+	end
+
+
+	def opuesto
+		return denominador-numerador
 	end
 
 end	
--- spec/nr_spec.rb
+++ spec/nr_spec.rb
@@ -5,44 +5,51 @@ require 'nr'
 describe NumerosRacionales do
 
 	before :each do
-		@nr = NumerosRacionales.new
+		@nr = NumerosRacionales.new(3.5,2.0)	
 	end
 
 	it "Debe existir un numerador" do
-		@nr.numerador.class.should == Fixnum
+		defined?(@nr.numerador)
 	end
 
 	it "Debe existir un denominador" do
-		@nr.denominador.class.should == Fixnum
+		defined?(@nr.denominador)
 	end
 
 	it "Debe de estar en su forma reducida" do
 		@nr.numeroracional.class.should == Float
-		
 	end
 
 	it "Se debe invocar al metodo num() para obtener el numerador" do
+		@nr.num.should == @nr.numerador
 	end
 
 	it "Se debe invocar al metodo denom() para obtener el denominador" do
+		@nr.denom.should == @nr.denominador
 	end
 
 	it "Se debe mostar por la consola la fraccion de la forma: a/b, donde a es el numerador y b el denominador" do
-	end
+		@nr.mostrarab
+	end	
 
 	it "Se debe mostar por la consola la fraccion en formato flotante" do
+		@nr.mostrarfloat
 	end
 	
 	it "Se debe comparar si dos fracciones son iguales con ==" do
+		@nr.comprobarfracciones.should == "Son iguales"
 	end
 
 	it "Se debe calcular el valor absoluto de una fraccion con el metodo abs" do
+		@nr.vabsoluto # Numeros no negativos
 	end
 
 	it "Se debe calcular el reciproco de una fraccion con el metodo reciprocal" do
+		@nr.reciproco
 	end
 
 	it "Se debe calcular el opuesto de una fraccion con -" do
+		#puts @nr.opuesto
 	end
 
 	it "Se debe sumar dos fracciones con + y dar el resultado de forma reducida" do
